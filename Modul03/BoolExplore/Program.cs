9d25611715613a491b8e6a1b944226f40a9c3947
Петър Димитров <pebodi@abv.bg>
2016-01-21 17:02:27 +0200

    Модул 03, Видео 06 - Работа с условни оператори

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -14,16 +14,22 @@ namespace BoolExplore
 
 			int _ui = Convert.ToInt32 (Console.ReadLine ());
 
-			bool _check01 = _ui <= _i.Length;
-			bool _check02 = _ui > 0;
-
-			bool _exp01 = _check01 && _check02; 	//Логическо И "&&"
-			bool _exp02 = _check01 || _check02; 	//Логическо ИЛИ " || "
-
-			Console.WriteLine ("Логическо И: " + _exp01.ToString ());
-			Console.WriteLine ("Логическо ИЛИ: " + _exp02.ToString ());
-
-			//Console.WriteLine ( _i [ _ui - 1]);
+//			bool _check01 = _ui <= _i.Length;
+//			bool _check02 = _ui > 0;
+//
+//			bool _exp01 = _check01 && _check02; 	//Логическо И "&&"
+//			bool _exp02 = _check01 || _check02; 	//Логическо ИЛИ " || "
+//			bool _exp03 = !_check01;
+//
+//			Console.WriteLine ("Логическо И: " + _exp01.ToString ());
+//			Console.WriteLine ("Логическо ИЛИ: " + _exp02.ToString ());
+//			Console.WriteLine ("Отрицание на " + _check01.ToString () + ":" + _exp03);
+			if (_ui > 0 && _ui <= _i.Length) {
+				Console.WriteLine (_i [_ui - 1]);
+			} else {
+				Console.WriteLine ("Невалиден индекс\n\n\n");
+			}
+			//
 		}
 	}
 }
